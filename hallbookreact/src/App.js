21043abf6a87777c670443849f43aa764e24d67a
nischalmainali21 <nischalmainali21@gmail.com>
2023-01-25 10:29:48 +0545

    Updated Navbar Using tailwind     Installed tailwind     Updated Navbar styling for large screens only

--- hallbookreact/src/App.js
+++ hallbookreact/src/App.js
@@ -2,6 +2,7 @@ import { Route,Routes } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Error from "./pages/Error"
+import Login from "./pages/Login"
 import Navbar from "./components/Navbar/Navbar"
 
 export default function App(){
@@ -13,6 +14,7 @@ export default function App(){
             <Routes>
                 <Route path="/" element={<Home />}/>
                 <Route path="/about" element={<About />} />
+                <Route path="/login" element={<Login />}></Route>
                 <Route path="*" element={<Error />}></Route> 
             </Routes> 
             
--- hallbookreact/src/components/Navbar/Navbar.js
+++ hallbookreact/src/components/Navbar/Navbar.js
@@ -2,46 +2,70 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { NavLink } from "react-router-dom";
 
-import "./Navbar.css"
+import "./Navbar.css";
 
-import { FaBars, FaTimes } from 'react-icons/fa';
-import { IconContext } from "react-icons/lib";
-import { RiBookletLine} from "react-icons/ri"
+import { FaBars, FaTimes } from "react-icons/fa";
 
-export default function Navbar(){
 
-    const [click,setClick] = useState(false);
+export default function Navbar() {
 
-    const handleClick = () => setClick(!click);
-    const closeMobileMenu = () => setClick(false);
+    const activeClassLink = "px-2 py-2.5 hover:bg-cprimary-300 hover:text-csecond-100 rounded-md transition bg-blue-200"
+    const normalClassLink = "px-2 py-2.5 hover:bg-cprimary-300 hover:text-csecond-100 rounded-md transition"
 
-    return(
-        <>
-        <IconContext.Provider value={{color:"#000"}}>
-        <nav className="navbar">
-            <div className="navbar-container">
-                <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-                    <RiBookletLine className="navbar-icon" />
-                    Hall Book
-                </Link>
-                <div className="menu-icon" onClick={handleClick}>
-                    {click? <FaTimes /> : <FaBars />}
-                </div>
-                <ul className={click? "navbar-menu active": "navbar-menu"}>
-                    <li className="navbar-item">
-                        <NavLink to="/" className={({isActive}) => "navbar-links" + (isActive? " active": "")}  onClick={closeMobileMenu}>
-                            Home
-                        </NavLink>
-                    </li>
-                    <li className="navbar-item">
-                        <NavLink to="/about" className={({isActive}) => "navbar-links" + (isActive? " active": "")}  onClick={closeMobileMenu}>
-                            About
-                        </NavLink>
-                    </li>
-                </ul>
+  return (  
+    <div>
+      <nav className="w-full rounded-sm bg-cprimary-100 px-8 py-1 text-lg text-cprimary-800 shadow-lg">
+        <div className="flex items-center justify-between">
+          {/* logo and primary nav */}
+          <div className="flex items-center space-x-10">
+            {/* logo */}
+            <div>
+              <Link to="/" className="flex items-center space-x-2 px-2 py-5">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="h-6 w-6 cursor-pointer text-blue-500"
+                >
+                  <path d="M3.375 3C2.339 3 1.5 3.84 1.5 4.875v.75c0 1.036.84 1.875 1.875 1.875h17.25c1.035 0 1.875-.84 1.875-1.875v-.75C22.5 3.839 21.66 3 20.625 3H3.375z" />
+                  <path
+                    fillRule="evenodd"
+                    d="M3.087 9l.54 9.176A3 3 0 006.62 21h10.757a3 3 0 002.995-2.824L20.913 9H3.087zm6.163 3.75A.75.75 0 0110 12h4a.75.75 0 010 1.5h-4a.75.75 0 01-.75-.75z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                <span className="font-bold">BookHall</span>
+              </Link>
             </div>
-        </nav>
-        </IconContext.Provider>
-        </>
-    )
+            {/* primary nav */}
+            <div>
+              <ul className="flex items-center space-x-6">
+                <li>
+                  <NavLink
+                    to="/"
+                    // className={`px-2 py-2.5 hover:bg-cprimary-300 hover:text-csecond-100 rounded-md transition ${({isActive})=>isActive?"bg-red-500":"bg-black-500"}`}
+                    // className={({isActive})=>isActive?"bg-red-500":"bg-blue-500"}
+                    // className={`${({isActive})=>isActive?"bg-red-500":"bg-blue-500"}`}
+                    className={({isActive})=>isActive?activeClassLink:normalClassLink}
+                
+                  >
+                    Home
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/about"
+                  className={({isActive})=>isActive?activeClassLink:normalClassLink}
+                  >About</NavLink>
+                </li>
+              </ul>
+            </div>
+          </div>
+          {/* secondary nav */}
+          <div>
+            <NavLink to="/login" className={({isActive})=>isActive?activeClassLink:normalClassLink}>Login</NavLink>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
 }

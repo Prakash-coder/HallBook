31e7185d8d1d72c9fe285e137543c00f3a17b3d4
nischalmainali21 <nischalmainali21@gmail.com>
2023-01-26 14:03:18 +0545

    Added Button to login page and changed components directory     Placed Input.js and LoginForm.js into the components/Login     Added Button element to the login page

--- HallBook/hallbookreact/src/pages/Login.js
+++ HallBook/hallbookreact/src/pages/Login.js
@@ -1,8 +1,17 @@
 import { Link } from "react-router-dom";
-import LoginForm from "../components/LoginForm";
+import LoginForm from "../components/Login/LoginForm";
+
+
+const loginBtnClass = `relative mx-auto block w-2/6 max-w-xs rounded-full bg-blue-500 px-6 py-4 text-base 
+font-medium uppercase leading-tight text-white shadow-md  transition duration-150 ease-in-out hover:bg-blue-700
+hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none
+focus:ring-0 active:bg-blue-800 active:shadow-lg md:mx-auto md:w-1/6 md:py-3`
 
 function Login() {
-  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //handle authentication of user login
+  };
 
   return (
     <div className=" min-h-screen">
@@ -37,8 +46,14 @@ function Login() {
       <LoginForm />
 
       {/* submit button */}
-
-
+      <button
+        type="action"
+        onSubmit={handleSubmit}
+        className={loginBtnClass}
+      >
+        Sign In
+      </button>
+      {/* submit button ends here */}
     </div>
   );
 }

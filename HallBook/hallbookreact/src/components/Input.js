1dcf42c2ae14e8e82b39b066b39a199a1308e900
nischalmainali21 <nischalmainali21@gmail.com>
2023-01-26 13:36:09 +0545

    Completed Header and Login Form     Built a header of the logo and name in the login file     build a reusable input component     Built a reusable loginForm component

---
+++ HallBook/hallbookreact/src/components/Input.js
@@ -0,0 +1,38 @@
+const fixedInputClass = `rounded-md appearance-none relative block w-full max-w-sm px-4 py-3  md:mx-auto md:w-2/6 mx-auto
+  border border-cprimary-500 placeholder-gray-500 text-cprimary-900 
+  focus:outline-none focus:ring-purple-500 focus:border-blue-500 focus:z-10 sm:text-sm
+  
+  `;
+
+function Input({
+  handleChange,
+  value,
+  labelText,
+  labelFor,
+  id,
+  name,
+  type,
+  isRequired = false,
+  placeholder,
+  customClass,
+}) {
+  return (
+    <div className="my-8">
+      <label htmlFor={labelFor} className="sr-only">
+        {labelText}
+      </label>
+      <input
+        onChange={handleChange}
+        value={value}
+        id={id}
+        name={name}
+        type={type}
+        required={isRequired}
+        className={fixedInputClass + customClass}
+        placeholder={placeholder}
+      />
+    </div>
+  );
+}
+
+export default Input;

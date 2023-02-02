e31320b7282bb66469581b2b7e56f9fa7beadbff
nischalmainali21 <nischalmainali21@gmail.com>
2023-02-02 19:35:20 +0545

    Changed the custom button styling

--- HallBook/hallbookreact/src/components/CButton.js
+++ HallBook/hallbookreact/src/components/CButton.js
@@ -1,9 +1,9 @@
 
 
-const loginBtnClass = `relative mx-auto block w-2/6 max-w-xs rounded-full bg-blue-500 px-6 py-4 text-base 
+const loginBtnClass = `relative mx-auto block rounded-full bg-blue-500 px-6 py-4 text-base 
 font-medium uppercase leading-tight text-white shadow-md  transition duration-150 ease-in-out hover:bg-blue-700
 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none
-focus:ring-0 active:bg-blue-800 active:shadow-lg md:mx-auto md:w-1/6 md:py-3`
+focus:ring-0 active:bg-blue-800 active:shadow-lg md:mx-auto md:py-3`
 
 function CButton(props) {
   return (

66f38b3aaea68e3452cb60f5b55cb76dad39cbdc
nischalmainali21 <nischalmainali21@gmail.com>
2023-02-02 19:35:42 +0545

    General Styling of the hallcard component

--- HallBook/hallbookreact/src/components/Hall/HallCard.js
+++ HallBook/hallbookreact/src/components/Hall/HallCard.js
@@ -5,20 +5,27 @@ function HallCard({
     capacity,
 }) {
   return (
-    <div>
-        <div>{name}</div>
-        <div>{capacity}</div>
+    <div className="shadow-lg rounded-lg">
+    <div className="flex flex-col w-full h-80 p-2">
+        <div className="text-3xl font-bold">{name}</div>
+        <div className="text-sm text-gray-500">{capacity}</div>
         {/* a div for the booked events and time intervals for those events needs to go here */}
+        <div className="flex">
+
         <CButton
         id="hall details"
         type="button"
         btnDesc="View Hall Details"
         />
+
         <CButton
         id="Book Hall"
         type="button"
         btnDesc="Book Hall"
         />
+
+        </div>
+    </div>
     </div>
   )
 }

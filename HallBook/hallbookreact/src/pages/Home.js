6ab031256cbda3f35fd41f1cd272eece9975522d
nischalmainali21 <nischalmainali21@gmail.com>
2023-02-02 19:21:56 +0545

    General Styling of the Home Page     Used some placeholder elements for testing

--- HallBook/hallbookreact/src/pages/Home.js
+++ HallBook/hallbookreact/src/pages/Home.js
@@ -11,13 +11,24 @@ const hallList = [
         name:"A Hall",
         capacity:"70",
     },
+    {
+        id:"3",
+        name:"B Hall",
+        capacity:"70",
+    },
+    {
+        id:"4",
+        name:"C Hall",
+        capacity:"70",
+    },
+
+
 
 ]
 
-
 export default function Home(){
     return (
-        <div>
+        <div className="flex flex-col w-2/3 mx-auto min-h-screen gap-8 mt-10 p-2">
             {hallList.map((hall)=>(
                 <HallCard
                 key={hall.id}
@@ -25,8 +36,6 @@ export default function Home(){
                 capacity={hall.capacity}
                 />
             ))}
-            
-            
         </div>
     )
 }

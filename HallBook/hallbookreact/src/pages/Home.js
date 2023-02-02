98dca94cd45ae6d33875d6a40a6d9df805ca5713
nischalmainali21 <nischalmainali21@gmail.com>
2023-02-02 18:20:07 +0545

    Rendered the hallcard component here     created a placeholder list of halls     Rendered single hall details

--- HallBook/hallbookreact/src/pages/Home.js
+++ HallBook/hallbookreact/src/pages/Home.js
@@ -1,10 +1,32 @@
-import { Link } from "react-router-dom";
+import HallCard from "../components/Hall/HallCard";
+
+const hallList = [
+    {
+        id:"1",
+        name:"Library Hall",
+        capacity:"150",
+    },
+    {
+        id:"2",
+        name:"A Hall",
+        capacity:"70",
+    },
+
+]
+
 
 export default function Home(){
     return (
         <div>
-            Home goes here
-            <Link to="about">Click to view the about page</Link>
+            {hallList.map((hall)=>(
+                <HallCard
+                key={hall.id}
+                name={hall.name}
+                capacity={hall.capacity}
+                />
+            ))}
+            
+            
         </div>
     )
 }

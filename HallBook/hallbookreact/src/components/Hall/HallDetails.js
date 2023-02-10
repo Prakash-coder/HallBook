d5aadb13d0b02cfb7d91dfc5cadb26f900d92d51
nischalmainali21 <nischalmainali21@gmail.com>
2023-02-10 10:22:31 +0545

    Render an ImageSlider component     Some general layout

--- HallBook/hallbookreact/src/components/Hall/HallDetails.js
+++ HallBook/hallbookreact/src/components/Hall/HallDetails.js
@@ -1,18 +1,21 @@
-import { useLocation } from "react-router-dom"
-
+import { useLocation } from "react-router-dom";
+import ImageSlider from "../ImageSlider";
 
 function HallDetails() {
-
-    const {state} = useLocation();
+  const { state } = useLocation();
 
   return (
-
     <>
-    {console.log(state)}
-    <div>{state.name}</div>
-    <div>{state.capacity}</div>
+      {console.log(state)}
+      <div className="mt-2 mx-auto min-h-screen w-full p-4 shadow-md">
+        <div className="text-3xl font-bold">{state.name}</div>
+        <div className="text-sm text-gray-500">Capacity: {state.capacity}</div>
+        <div className="w-full h-96 mt-6">
+          <ImageSlider slides={state.slides}/>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default HallDetails
+export default HallDetails;

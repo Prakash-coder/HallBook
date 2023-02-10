5ce9fdff236c4919f4273fa13cad0c051df72aad
nischalmainali21 <nischalmainali21@gmail.com>
2023-02-10 10:21:55 +0545

    Added Id and slides property to the incoming objec hall

--- HallBook/hallbookreact/src/components/Hall/HallCard.js
+++ HallBook/hallbookreact/src/components/Hall/HallCard.js
@@ -1,58 +1,47 @@
-import CButton from "../CButton"
-import {useNavigate } from "react-router-dom"
+import CButton from "../CButton";
+import { useNavigate } from "react-router-dom";
 
+function HallCard({ id,name, capacity,slides }) {
+  const hall = { id,name, capacity,slides };
 
-
-
-
-function HallCard({
-    name,
-    capacity,
-}) {
-
-  const hall = {name,capacity}
-
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const navigateToHall = (hall) => {
-    console.log("navigating to hall details")
-    let path = `/halldetail/${hall.name}`
-    navigate(path,{state:{...hall}})
-  }
+    console.log("navigating to hall details");
+    let path = `/halldetail/${hall.name}`;
+    navigate(path, { state: { ...hall } });
+  };
 
   const navigateToBookHall = (hall) => {
-    console.log("navigating to book hall")
-    let path = `/bookhall/${hall.name}`
-    navigate(path,{state:{...hall}})
-  }
-  
+    console.log("navigating to book hall");
+    let path = `/bookhall/${hall.name}`;
+    navigate(path, { state: { ...hall } });
+  };
 
   return (
-    <div className="shadow-lg rounded-lg">
-    <div className="flex flex-col w-full h-80 p-2">
+    <div className="rounded-lg shadow-lg">
+      <div className="flex h-80 w-full flex-col p-2">
         <div className="text-3xl font-bold">{name}</div>
         <div className="text-sm text-gray-500">{capacity}</div>
         {/* a div for the booked events and time intervals for those events needs to go here */}
-        <div className="flex flex-col md:flex-row gap-6">
+        <div className="flex flex-col gap-6 md:flex-row">
+          <CButton
+            id="hall details"
+            type="button"
+            btnDesc="View Hall Details"
+            onClick={() => navigateToHall(hall)}
+          />
 
-        <CButton
-        id="hall details"
-        type="button"
-        btnDesc="View Hall Details"
-        onClick = {() => navigateToHall(hall)}
-        />
-
-        <CButton
-        id="Book Hall"
-        type="button"
-        btnDesc="Book Hall"
-        onClick = {()=> navigateToBookHall(hall)}
-        />
-
+          <CButton
+            id="Book Hall"
+            type="button"
+            btnDesc="Book Hall"
+            onClick={() => navigateToBookHall(hall)}
+          />
         </div>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default HallCard
+export default HallCard;

6376c5970b0d3be34b5fd588150ce11cd0ed8a5e
nischalmainali21 <nischalmainali21@gmail.com>
2023-02-11 14:38:12 +0545

    Updated general layout of BookHall page

--- HallBook/hallbookreact/src/components/Hall/BookHall.js
+++ HallBook/hallbookreact/src/components/Hall/BookHall.js
@@ -8,8 +8,11 @@ function BookHall() {
   return (
     <>
     {console.log(state)}
-    <div>{state.name}</div>
-    <div>{state.capacity}</div>
+    
+    <div className="mx-auto mt-6 min-h-screen w-full max-w-4xl p-4 shadow-lg">
+        <div className="text-3xl font-bold">{state.name}</div>
+        <div className="text-sm text-gray-500">Capacity: {state.capacity}</div>
+      </div>
     </>
   )
 }

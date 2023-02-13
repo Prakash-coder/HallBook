825f41dc9ed6b555d20f3530bc31596af13accc9
nischalmainali21 <nischalmainali21@gmail.com>
2023-02-13 12:57:25 +0545

    Rendered the datepicker component

--- HallBook/hallbookreact/src/components/Hall/BookHall.js
+++ HallBook/hallbookreact/src/components/Hall/BookHall.js
@@ -1,35 +1,22 @@
-import { useState } from "react";
-import { useLocation } from "react-router-dom";
 
-import HallInpput from "./HallInpput";
+import { useLocation } from "react-router-dom";
+import DatePicker from "../DatePicker";
+
+
 
 function BookHall() {
   const { state } = useLocation();
 
-  const [date, setDate] = useState("");
-  console.log(date);
-  const handleChange = (e) => {
-    setDate(e.target.value);
-  };
-
   return (
     <>
-      {console.log(state)}
-
       <div className="mx-auto mt-6 min-h-screen w-full max-w-4xl p-4 shadow-lg">
         <div className="text-3xl font-bold">{state.name}</div>
         <div className="text-sm text-gray-500">Capacity: {state.capacity}</div>
         <form>
           {/* date input  */}
-          <HallInpput
-          key = "eventDate"
-          id = "eventDate"
-          type = "date"
-          value={date}
-          isRequired = {true}
-          onChange = {handleChange}
-          />
+          <DatePicker />
           {/* date input ends here */}
+          
         </form>
       </div>
     </>

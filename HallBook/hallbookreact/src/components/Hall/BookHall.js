9ec41d2132dde569269cfd5faf97b2d11ef85451
nischalmainali21 <nischalmainali21@gmail.com>
2023-02-13 10:22:49 +0545

    Created a event date input in bookhall     Created a HallInpput component to render input element

--- HallBook/hallbookreact/src/components/Hall/BookHall.js
+++ HallBook/hallbookreact/src/components/Hall/BookHall.js
@@ -1,20 +1,39 @@
-import { useLocation } from "react-router-dom"
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
 
+import HallInpput from "./HallInpput";
 
 function BookHall() {
+  const { state } = useLocation();
 
-    const {state} = useLocation();
+  const [date, setDate] = useState("");
+  console.log(date);
+  const handleChange = (e) => {
+    setDate(e.target.value);
+  };
 
   return (
     <>
-    {console.log(state)}
-    
-    <div className="mx-auto mt-6 min-h-screen w-full max-w-4xl p-4 shadow-lg">
+      {console.log(state)}
+
+      <div className="mx-auto mt-6 min-h-screen w-full max-w-4xl p-4 shadow-lg">
         <div className="text-3xl font-bold">{state.name}</div>
         <div className="text-sm text-gray-500">Capacity: {state.capacity}</div>
+        <form>
+          {/* date input  */}
+          <HallInpput
+          key = "eventDate"
+          id = "eventDate"
+          type = "date"
+          value={date}
+          isRequired = {true}
+          onChange = {handleChange}
+          />
+          {/* date input ends here */}
+        </form>
       </div>
     </>
-  )
+  );
 }
 
-export default BookHall
+export default BookHall;

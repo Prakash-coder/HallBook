8e35f5ef9372a7598c01911635f25c645b9db9b8
nischalmainali21 <nischalmainali21@gmail.com>
2023-02-14 12:36:31 +0545

    Added handelSubmit functionality

--- HallBook/hallbookreact/src/components/Hall/BookHall.js
+++ HallBook/hallbookreact/src/components/Hall/BookHall.js
@@ -4,15 +4,22 @@ import DatePicker from "../DatePicker";
 function BookHall() {
   const { state } = useLocation();
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target.value)
+  };
+
   return (
     <>
       <div className="mx-auto mt-6 min-h-screen w-full max-w-4xl p-4 shadow-lg">
         <div className="text-3xl font-bold">{state.name}</div>
         <div className="text-sm text-gray-500">Capacity: {state.capacity}</div>
-        <form>
+        <form onSubmit={handleSubmit}>
           {/* date input  */}
           <DatePicker />
           {/* date input ends here */}
+
+          <button type="submit">Submit</button>
         </form>
       </div>
     </>

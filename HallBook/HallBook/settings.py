a045cc85d31405d1d770e645b635cd2a79fd4330
nischalmainali21 <nischalmainali21@gmail.com>
2023-02-18 09:53:25 +0545

    Updated middlewares and cors_allowed_origins for same origin access

--- HallBook/HallBook/settings.py
+++ HallBook/HallBook/settings.py
@@ -50,11 +50,17 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
 ]
 
 ROOT_URLCONF = 'HallBook.urls'
@@ -85,10 +91,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'hallbook',
-        'USER': 'hallbookuser',
-        'PASSWORD': 'password',
+        'USER': 'postgres',
+        'PASSWORD': '8630',
         'HOST': 'localhost',
-        'PORT': '5434',
+        'PORT': '5432',
     }
 }
 
